4227988ff52d22ae4d05f42106a86882a6cb68d9
Shekhar Kumar Gupta <shekhar114@gmail.com>
2014-07-17 23:11:53 +0530

    SolveMeFirst solved

--- src/SolveMeFirst/SolveMeFirst.java
+++ src/SolveMeFirst/SolveMeFirst.java
@@ -1,7 +1,23 @@
 package SolveMeFirst;
 
+import java.util.Scanner;
+
 /**
  * Created by SKG on 17-Jul-14.
  */
-public class SolveMeFirst {
+public class SolveMeFirst {static int solveMeFirst(int a, int b) {
+  return a+b;
+}
+
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int _a;
+    _a = in.nextInt();
+    int _b;
+    _b = in.nextInt();
+    int sum;
+    sum = solveMeFirst(_a, _b);
+    System.out.println(sum);
+  }
 }
