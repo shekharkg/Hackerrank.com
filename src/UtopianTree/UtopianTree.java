108928f23e1bdf6f8322ffe2873b6f31b63758fd
Shekhar Kumar Gupta <shekhar114@gmail.com>
2014-07-21 11:35:38 +0530

    Utopian Tree Solved

---
+++ src/UtopianTree/UtopianTree.java
@@ -0,0 +1,39 @@
+package UtopianTree;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Created by SKG on 21-Jul-14.
+ */
+public class UtopianTree {
+  public static void main(String args[]){
+    Scanner scanner = new Scanner(new InputStreamReader(System.in));
+    int testCaseCount = scanner.nextInt();
+    int[] textCase = new int[testCaseCount];
+    for(int i=0; i<testCaseCount; i++){
+      int treeHeight = 1;
+      textCase[i] = scanner.nextInt();
+      int j = 0;
+      if(textCase[i]%2==0){
+        while (j<textCase[i]){
+          treeHeight = treeHeight * 2;
+          j++;
+          treeHeight++;
+          j++;
+        }
+        System.out.println(treeHeight);
+      }
+      else{
+        while (j<textCase[i]-1){
+          treeHeight = treeHeight * 2;
+          j++;
+          treeHeight++;
+          j++;
+        }
+        treeHeight = treeHeight * 2;
+        System.out.println(treeHeight);
+      }
+    }
+  }
+}
